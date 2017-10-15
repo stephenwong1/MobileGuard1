c69573c32c2769fc8ba64bdc2d70ec598a53f242
stephenwong1 <1071105989@qq.com>
2017-10-15 18:15:01 +0800

    10.15

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup1Activty.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup1Activty.java
@@ -1,8 +1,29 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
 /**
  * Created by hitma on 2017/10/15.
  */
 
 public class Setup1Activty extends BaseSetupActivity {
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_1);
+
+        ((RadioButton)findViewById(R.id.rb_first)).setChecked(true);
+
+    }
+    public  void showNext(){
+        startActivityAndFinishSelf(Setup2Activity.class);
+    }
+
+    public  void  showPre(){
+        Toast.makeText(this,"当前页面已经是第一页",Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
@@ -1,8 +1,26 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
 /**
  * Created by hitma on 2017/10/15.
  */
 
 public class Setup2Activity extends BaseSetupActivity {
+
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_2);
+        ((RadioButton)findViewById(R.id.rb_second)).setChecked(true);
+    }
+
+    public void showNext(){
+        startActivityAndFinishSelf(Setup3Activity.class);
+    }
+    public  void  showPre(){
+        startActivityAndFinishSelf(Setup1Activty.class);
+    }
 }
