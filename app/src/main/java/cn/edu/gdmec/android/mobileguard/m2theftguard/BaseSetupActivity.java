76fd5423c1fbe1ff015b4fdc022de2c7b71189a8
stephenwong1 <1071105989@qq.com>
2017-10-15 13:16:39 +0800

    10.15

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/BaseSetupActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/BaseSetupActivity.java
@@ -1,8 +1,16 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+
 /**
  * Created by hitma on 2017/10/15.
  */
 
-public abstract class BaseSetupActivity {
+public abstract class BaseSetupActivity extends AppCompatActivity{
+    public SharedPreferences sp;
+    private GestureDetector mGestureDetector;
+    public abstract  void showNext();
+    public  abstract  void  showPre();
 }
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup1Activty.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard;
+
+/**
+ * Created by hitma on 2017/10/15.
+ */
+
+public class Setup1Activty extends BaseSetupActivity {
+}
